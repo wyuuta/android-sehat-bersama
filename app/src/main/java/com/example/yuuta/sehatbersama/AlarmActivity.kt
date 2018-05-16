4559831b8700c68bdc26a69a03fb2fab27d0183b
wyuuta <wilbudewa2@yahoo.com>
2018-05-16 19:34:45 +0700

    add alarm view

---
+++ app/src/main/java/com/example/yuuta/sehatbersama/AlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yuuta.sehatbersama
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AlarmActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+}
