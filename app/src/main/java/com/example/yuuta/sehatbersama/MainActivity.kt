029338c32045e74d0713edd6ee8da547cf35a502
William AD <w.albertusd@gmail.com>
2018-05-16 17:00:07 +0700

    prepare for course activity

--- app/src/main/java/com/example/yuuta/sehatbersama/MainActivity.kt
+++ app/src/main/java/com/example/yuuta/sehatbersama/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.yuuta.sehatbersama
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun detailPage(id: Int) {
+        val courseIntent = Intent(this, MainActivity::class.java)
+        startActivity(courseIntent)
+    }
+
+    fun chestCourse(view: View) {
+        detailPage(1)
+    }
+
+    fun absCourse(view: View) {
+        detailPage(2)
+    }
+
+    fun armCourse(view: View) {
+        detailPage(3)
+    }
+
+    fun legCourse(view: View) {
+        detailPage(4)
+    }
+
+    fun shoulderCourse(view: View) {
+        detailPage(5)
+    }
+
 }
